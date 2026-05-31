b06b9d9723e7b009223b383c157ad0bdca331aa9
Vijayadurga200 <vijayadirgachandan@gmail.com>
2026-05-31 18:28:51 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
 $host = "mysql-6ca1d4f-vijayadurgachandana7-1cae.j.aivencloud.com";
-$port = 27447;
-$dbname = "defaultdb";
+$port = 3306;
+$dbname = "hotel_db";
 $username = "avnadmin";
-$password = "AVNS_1EO2y8s-g1dQKO-HBLE";
+$password = "Root@123";
 
 try {
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
