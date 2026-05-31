a1f102df67261d446150a9a8715b84f920a9f484
Vijayadurga200 <vijayadirgachandan@gmail.com>
2026-05-31 18:08:45 +0530

    Refactor PDO connection setup in db.php

--- db.php
+++ db.php
@@ -3,20 +3,14 @@ $host = "mysql-6ca1d4f-vijayadurgachandana7-1cae.j.aivencloud.com";
 $port = 27447;
 $dbname = "defaultdb";
 $username = "avnadmin";
-$password = "AVNS_1EO2y8s-g1dQKO-HBLE"; // paste from Aiven
+$password = "AVNS_1EO2y8s-g1dQKO-HBLE";
 
 try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8",
-        $username,
-        $password,
-        [
-            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
-        ]
-    );
+    $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
+
+    $pdo = new PDO($dsn, $username, $password);
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
